bb9b60aaeb8147292f7076edd076f2c708619dea
leejiyeon <sky75013@naver.com>
2025-08-21 09:45:37 +0900

    v1

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,14 +1,41 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext(String basePackage) {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationContext {
+    private Map<String, Object> beans;
+    private String basePackage;
+
+    public ApplicationContext(String basePackage) {
+        this.basePackage = basePackage;
+        beans = new HashMap<>();
     }
 
     public void init() {
     }
 
     public <T> T genBean(String beanName) {
-        return null;
+
+        Object bean = beans.get(beanName);
+        if (bean == null) {
+            bean = switch (beanName) {
+                case "testFacadePostService" -> new TestFacadePostService(
+                        genBean("testPostService"),
+                        genBean("testPostRepository")
+                );
+                case "testPostService" -> new TestPostService(
+                        genBean("testPostRepository")
+                );
+                case "testPostRepository" -> new TestPostRepository();
+                default -> null;
+            };
+            beans.put(beanName, bean);
+        }
+        return (T) bean;
     }
 }
